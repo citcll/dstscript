37bada780c59c7de59482a5775cb7d27739fb3be
ariwori <ariwori@wqlin.com>
2018-10-16 16:39:22 +0800

    update

--- test.sh
+++ test.sh
@@ -4,3 +4,18 @@ date
 cd ${HOME}/steamcmd || exit 1
 ./steamcmd.sh +login "anonymous" +force_install_dir "${dst_server_dir}" +app_update "343050" validate +quit
 date
+
+# Gets availablebuild info
+	cd "${steamcmddir}" || exit
+	availablebuild=$(./steamcmd.sh +login "${steamuser}" "${steampass}" +app_info_update 1 +app_info_print "${appid}" +app_info_print "${appid}" +quit | sed -n '/branch/,$p' | grep -m 1 buildid | tr -cd '[:digit:]')
+	if [ -z "${availablebuild}" ]; then
+		fn_print_fail "Checking for update: SteamCMD"
+		sleep 0.5
+		fn_print_fail_nl "Checking for update: SteamCMD: Not returning version info"
+		fn_script_log_fatal "Checking for update: SteamCMD: Not returning version info"
+		core_exit.sh
+	else
+		fn_print_ok "Checking for update: SteamCMD"
+		fn_script_log_pass "Checking for update: SteamCMD"
+		sleep 0.5
+	fi
