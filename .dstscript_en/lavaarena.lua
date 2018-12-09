03bb4e87a2f101a52488ee3ff2165cec94d5a804
ariwori <linweiqing2016@gmail.com>
2018-12-09 18:05:17 +0800

    update english

--- .dstscript_en/lavaarena.lua
+++ .dstscript_en/lavaarena.lua
@@ -1,12 +1,12 @@
 return {
     background_node_range={ 0, 1 },
-    desc="由最好用的脚本dstserver.sh创建！",
+    desc="create by dstserver.sh!",
     hideminimap=false,
     id="LAVAARENA",
     location="lavaarena",
     max_playlist_position=999,
     min_playlist_position=0,
-    name="熔炉竞技场--由脚本dstserver.sh创建！",
+    name="Forge MOD--create by dstserver.sh!",
     numrandom_set_pieces=0,
     override_level_string=false,
     overrides={
