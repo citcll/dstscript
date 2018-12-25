fa3a65aedd027adeaed045283553d1a5f84641b9
ariwori <linweiqing2016@gmail.com>
2018-12-25 20:29:13 +0800

    update

--- dstserver.sh
+++ dstserver.sh
@@ -5,7 +5,7 @@
 #    Author: Ariwori
 #    Blog: https://wqlin.com
 #===============================================================================
-script_ver="2.2.4"
+script_ver="2.2.5"
 dst_conf_dirname="DoNotStarveTogether"
 dst_conf_basedir="${HOME}/.klei"
 dst_base_dir="${dst_conf_basedir}/${dst_conf_dirname}"
